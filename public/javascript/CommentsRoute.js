cddef2cdd59123d0a950753f3cde0ded69c50d8f
Prateek <Prateekj@thoughtworks.com>
2013-11-19 14:07:12 +0530

    Version : 1.4 Latest version, with sign up and home page

--- public/javascript/CommentsRoute.js
+++ public/javascript/CommentsRoute.js
@@ -4,14 +4,22 @@ var url=require('url');
  var querystring = require("querystring"); 
 var commentsArray=fs.readFileSync('comments.txt','utf-8') && JSON.parse(fs.readFileSync('comments.txt',"utf-8")) || [];
 var htmlFileData=fs.readFileSync('./public/clientPage.html','utf-8');
+var home=fs.readFileSync('./public/Home.html','utf-8');
 var login=fs.readFileSync('./public/Login.html','utf-8');
 var signUp=fs.readFileSync('./public/signUp.html','utf-8');
+var contact=fs.readFileSync('./public/contact.html','utf-8');
 var cssForSignUpandLogin=fs.readFileSync('./public/stylesheets/mystyle.css');
 var cssAfterLogin=fs.readFileSync('./public/stylesheets/style.css');
 var step=fs.readFileSync('./Photos/step.jpg');
 var banner=fs.readFileSync('./public/images/banner.png');
 var banner2=fs.readFileSync('./public/images/banner2.png');
 var logo=fs.readFileSync('./public/images/logo.png');
+var logo2=fs.readFileSync('./public/images/logo2.png');
+var viewmap=fs.readFileSync('./public/images/map.jpg')
+var img1=fs.readFileSync('./public/images/img1.jpg');
+var img2=fs.readFileSync('./public/images/img2.jpg');
+var img3=fs.readFileSync('./public/images/img3.png');
+var img4=fs.readFileSync('./public/images/img4.jpg');
 var favicon=fs.readFileSync('./public/images/icon2.ico');
 var background=fs.readFileSync('./public/images/bg.jpg');
 var peoplesInfo=fs.readFileSync('PeopleInfo.txt','utf-8') && JSON.parse(fs.readFileSync('PeopleInfo.txt','utf-8')) || [];
@@ -41,7 +49,7 @@ var renderer=function(response,type,text){
 
 handler['/']=function(request,response){
 	var listComment=GetReadableCommentsFromObject(commentsArray);
-	renderer(response,ContentType.html,login);
+	renderer(response,ContentType.html,home);
 }
 
 var credentialsMatch=function(email,password){
@@ -78,12 +86,30 @@ var GetUserDataObject=function(postData){
 }
 
 var Validate=function(InfoObj,response){
-	if(!InfoObj.email.match('@')){
+	var emailPattern=/^[_A-Za-z0-9]+@commentbox.com/;
+	var namePattern=/^[0-9]/;
+	if(InfoObj.name==''){
+		renderer(response,ContentType.html,'<h1>You can\'t register without Your name.</h1>');
+		return false;
+	}
+	if(InfoObj.name.match(namePattern)){
+		renderer(response,ContentType.html,'<h1>Name can\'t be start with Number.</h1>');
+		return false;
+	}
+	if(!InfoObj.email.match(emailPattern)){
 		renderer(response,ContentType.html,'<h1>Invalid Mail</h1>');
 		return false;
 	}
+	if(InfoObj.password=='' && InfoObj.cpassword==''){
+		renderer(response,ContentType.html,'<h1>Password field can\'t be empty.</h1>');
+		return false;	
+	}
 	if(InfoObj.password != InfoObj.cpassword){
 		renderer(response,ContentType.html,'<h1>Password doesn\'t match</h1>');
+		return false;	
+	}
+	if(InfoObj.password.length <= 8){
+		renderer(response,ContentType.html,'<h1>Password should be more than 8 characters. </h1>');
 		return false;	
 	}
 	return true;
@@ -114,11 +140,17 @@ handler['/ToRegisterUser']=function(request,response){
 	})
 }
 
+handler['/contact.html']=function(request,response){
+	renderer(response,ContentType.html,contact);
+}
 
 handler['/signUp.html']=function(request,response){
 	renderer(response,ContentType.html,signUp);
 }
 
+handler['/Login.html']=function(request,response){
+	renderer(response,ContentType.html,login);
+}
 
 handler['/clientPage.html']=function(request,response){
 	request.setEncoding('utf8');
@@ -133,24 +165,48 @@ handler['/clientPage.html']=function(request,response){
 	});
 }
 
+handler['/images/map.jpg']=function(request,response){
+	renderer(response,ContentType.imgJpg,viewmap);
+}
+
+handler['/img1.jpg']=function(request,response){
+	renderer(response,ContentType.imgJpg,img1);
+}
+
+handler['/img2.jpg']=function(request,response){
+	renderer(response,ContentType.imgJpg,img2);
+}
+
+handler['/img3.png']=function(request,response){
+	renderer(response,ContentType.imgPng,img3);
+}
+
+handler['/img4.jpg']=function(request,response){
+	renderer(response,ContentType.imgJpg,img4);
+}
+
 handler['/images/step.jpg']=function(request,response){
 	renderer(response,ContentType.imgJpg,step);
 }
 
-handler['/images/bg.jpg']=function(request,response){
+handler['/bg.jpg']=function(request,response){
 	renderer(response,ContentType.imgJpg,background);
 }
 
-handler['/images/banner.png']=function(request,response){
+handler['/banner.png']=function(request,response){
 	renderer(response,ContentType.imgPng,banner);
 }
 
-handler['/images/banner2.png']=function(request,response){
+handler['/banner2.png']=function(request,response){
 	renderer(response,ContentType.imgPng,banner2);
 }
 
-handler['/images/logo.png']=function(request,response){
+handler['/logo.png']=function(request,response){
 	renderer(response,ContentType.imgPng,logo);
+}
+
+handler['/logo2.png']=function(request,response){
+	renderer(response,ContentType.imgPng,logo2);
 }
 
 handler['/stylesheets/mystyle.css']=function(request,response){
